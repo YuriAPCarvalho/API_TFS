245480a4dc1eeda959bb0fc02ada6c9484fbb264
Fillipe Kenzo <fillipekenzo123@gmail.com>
2025-02-11 14:17:10 -0400

    Ajuste interation

--- src/app/components/CadastrarTask/CadastrarTask.tsx
+++ src/app/components/CadastrarTask/CadastrarTask.tsx
@@ -81,7 +81,17 @@ export default function CadastrarTask() {
 
   const times = [{ value: "g08", label: "G08" }];
   const sprints = [{ value: "Sprint 216", label: "Sprint 216" }];
-  const integrantes = [{ value: "Fillipe Kenzo Yamasaki Sawamura", label: "Fillipe Kenzo Yamasaki Sawamura" }];
+  const integrantes = [
+    { value: "Alan Arguelho da Silva", label: "Alan Arguelho da Silva" },
+    { value: "Bruno Xavier Rodrigues", label: "Bruno Xavier Rodrigues" },
+    { value: "Eduardo Silva Arcanjo", label: "Eduardo Silva Arcanjo" },
+    { value: "Fabricio Almeida de Oliveira", label: "Fabricio Almeida de Oliveira" },
+    { value: "Fillipe Kenzo Yamasaki Sawamura", label: "Fillipe Kenzo Yamasaki Sawamura" },
+    { value: "Gabriel Medeiros Gomes da Silva", label: "Gabriel Medeiros Gomes da Silva" },
+    { value: "Higor Henrique Campos de Assis", label: "Higor Henrique Campos de Assis" },
+    { value: "Marcel Ferreira Yassumoto", label: "Marcel Ferreira Yassumotoy" },
+    { value: "Yuri Alexandre Pires de Carvalho", label: "Yuri Alexandre Pires de Carvalho" },
+  ];
 
   const handleSubmit = async (values: any) => {
     console.log(values);
@@ -145,7 +155,7 @@ export default function CadastrarTask() {
             {
               op: "add",
               path: "/fields/System.State",
-              value: "To Do",
+              value: "Done",
             },
             {
               op: "add",
@@ -155,7 +165,7 @@ export default function CadastrarTask() {
             {
               op: "add",
               path: "/fields/System.IterationPath",
-              value: `${project}\\${formValues.sprint}`,
+              value: `${project}\\Área de Negócios\${formValues.sprint}`,
             },
             {
               op: "add",
@@ -190,6 +200,11 @@ export default function CadastrarTask() {
             {
               op: "add",
               path: "/fields/Custom.SGI.DataExecucao",
+              value:  d,
+            },
+            {
+              op: "add",
+              path: "/fields/Microsoft.VSTS.Common.ClosedDate",
               value:  d,
             },
           ]);
