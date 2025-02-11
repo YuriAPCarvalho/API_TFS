5e5104b8defc2ccc3780510c834c471fae42cf96
Yuri Alexandre Pires de Carvalho <yuri.apcarvalho@gmail.com>
2025-02-11 15:46:49 -0300

    Ajuste bordão e copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { FaRocket } from "react-icons/fa";
 import { LogoSistema } from "./components/LogoSistema/LogoSistema";
 import Styles from "./styles.module.scss";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
@@ -13,24 +13,26 @@ export default function Home() {
         <div className={Styles.container}>
           <div className={Styles.text_section}>
             <h1 className={Styles.title}>
-              Lançamentos de <span className={Styles.highlight}>UST</span>
+              Lançamento de <span className={Styles.highlight}>UST</span>
             </h1>
             <p className={Styles.texto}>
-              Desenvolvido para apoiar e conduzir equipes a lugares que ainda
-              não alcançados!
+              Simplificando tarefas e acelerando resultados.
             </p>
             <button
               className={Styles.launch_button}
               onClick={() => router.push("/formulario")}
             >
               <FaRocket />
-              Iniciar Lançamento
+              Começe agora!
             </button>
           </div>
           <div className={Styles.image_section}>
             <LogoSistema className={Styles.logoSistema} />
           </div>
         </div>
+        <footer className={Styles.footer}>
+          <p>&copy; CSIC - Time G08</p>
+        </footer>
       </div>
     </>
   );
